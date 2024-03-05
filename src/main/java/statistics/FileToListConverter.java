b39360b29c74d0ebd76789625ee0057366010720
olivertate9 <kolomytsev95@gmail.com>
2024-03-05 11:08:58 +0200

    minor changes

--- src/main/java/statistics/FileToListConverter.java
+++ src/main/java/statistics/FileToListConverter.java
@@ -1,26 +1,19 @@
 package statistics;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileToListConverter {
     public List<Long> getListFromFile(String fileName) {
-        ClassLoader classLoader = getClass().getClassLoader();
         List<Long> allNumbers = new ArrayList<>();
 
-        try (InputStream stream = classLoader.getResourceAsStream(fileName);
-             InputStreamReader streamReader = new InputStreamReader(stream);
-             BufferedReader reader = new BufferedReader(streamReader)) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 allNumbers.add(Long.parseLong(line));
             }
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
